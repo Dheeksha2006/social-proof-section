cb09124c87f60e779785ccd8d5a90de49222067a
kaoutar-ouadih <kaoutar.ouadih9@gmail.com>
2025-01-23 23:21:59 +0100

    social proof section

---
+++ src/App.tsx
@@ -0,0 +1,96 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='p-20 bg-[url("/assets/bg-pattern-top-desktop.svg")] bg-left-top bg-no-repeat'>
+      <div className='flex flex-col lg:flex-row gap-10 mb-8'>
+        <div className='lg:w-1/2 text-center lg:text-left'>
+          <h1 className='text-4xl font-bold text-[hsl(300,43%,22%)] w-full lg:w-[80%]'>10,000+ of our users love our products.</h1>
+          <p className='mt-3 text-[hsl(303,10%,53%)] w-full lg:w-[90%]'>We only provide great products combined with excellent customer service.
+          See what our satisfied customers are saying about our services.</p>
+        </div>
+        <div className='lg:w-1/2 mt-10'>
+          <div className='flex flex-col lg:flex-row gap-6 items-center bg-[hsl(300,24%,96%)] py-3 px-4 rounded-md mb-4 w-full lg:w-[80%] text-sm'>
+            <div className='flex gap-1'>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+            </div>
+            <span className='text-[hsl(300,43%,22%)] font-bold text-sm'>Rated 5 Stars in Reviews</span>
+          </div>
+          <div className='flex flex-col lg:flex-row gap-6 items-center bg-[hsl(300,24%,96%)] py-3 px-4 rounded-md mb-4 relative left-0 lg:left-10 w-full lg:w-[80%] text-sm'>
+            <div className='flex gap-1'>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+            </div>
+            <span className='text-[hsl(300,43%,22%)] font-bold'>Rated 5 Stars in Report Guru</span>
+          </div>
+          <div className='flex flex-col lg:flex-row gap-6 items-center bg-[hsl(300,24%,96%)] py-3 px-4 rounded-md mb-4 relative left-0 lg:left-20 w-full lg:w-[80%] text-sm'>
+            <div className='flex gap-1'>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+              <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M16.539 6.097a.297.297 0 00-.24-.202l-5.36-.779L8.542.26a.296.296 0 00-.53 0L5.613 5.117l-5.36.779a.297.297 0 00-.165.505l3.88 3.78-.917 5.34a.297.297 0 00.43.312l4.795-2.52 4.794 2.52a.296.296 0 00.43-.313l-.916-5.338L16.464 6.4c.08-.08.11-.197.075-.304z" fill="#EF9546" fill-rule="nonzero"/></svg>
+            </div>
+            <span className='text-[hsl(300,43%,22%)] font-bold'>Rated 5 Stars in BestTech</span>
+          </div>
+        </div>
+      </div>
+      <div className='grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3'>
+        {/* card 1 */}
+        <div className='bg-[hsl(300,43%,22%)] rounded-md p-8 text-white'>
+            <div className='flex gap-4 mb-5'>
+              <img src="/assets/image-colton.jpg" alt="profile image" className='rounded-full w-12 ' />
+              <div className='flex flex-col text-sm font-bold'>
+                <span >Colton Smith</span>
+                <span className='text-[hsl(333,80%,67%)]'>Verified Buyer</span>
+              </div>
+            </div>
+          <p className='text-sm'>
+            "We needed the same printed design as the one we had ordered a week prior.
+            Not only did they find the original order, but we also received it in time.
+            Excellent!"
+          </p>
+        </div>
+        {/* card 2 */}
+        <div className='bg-[hsl(300,43%,22%)] rounded-md p-8 text-white relative top-6'>
+            <div className='flex gap-4 mb-5'>
+              <img src="/assets/image-irene.jpg" alt="profile image" className='rounded-full w-12 ' />
+              <div className='flex flex-col text-sm font-bold'>
+                <span >Irene Roberts</span>
+                <span className='text-[hsl(333,80%,67%)]'>Verified Buyer</span>
+              </div>
+            </div>
+          <p className='text-sm'>
+            "Customer service is always excellent and very quick turn around. Completely
+            delighted with the simplicity of the purchase and the speed of delivery."
+          </p>
+        </div>
+        {/* card 3 */}
+        <div className='bg-[hsl(300,43%,22%)] rounded-md p-8 text-white relative top-12'>
+            <div className='flex gap-4 mb-5'>
+              <img src="/assets/image-anne.jpg" alt="profile image" className='rounded-full w-12 ' />
+              <div className='flex flex-col text-sm font-bold'>
+                <span >Anne Wallace</span>
+                <span className='text-[hsl(333,80%,67%)]'>Verified Buyer</span>
+              </div>
+            </div>
+          <p className='text-sm'>
+            "Put an order with this company and can only praise them for the very high
+            standard. Will definitely use them again and recommend them to everyone!"
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
